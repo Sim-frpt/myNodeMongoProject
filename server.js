b8c799a3770a021c54947938a7eac497c58eeea4
Sim <simon.fraipont@gmail.com>
2019-06-04 14:32:28 +0200

    added nodemon functionality

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const port = 8080;
 app.set('view engine', 'mustache');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello man!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
